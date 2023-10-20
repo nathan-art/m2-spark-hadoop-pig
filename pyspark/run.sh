ad3f4aec97cc1752b8981e0182674a7091a4edb8
nathan-art <58117140+nathan-art@users.noreply.github.com>
2023-10-20 16:28:02 +0200

    saves pyspark results in the bucket

--- pyspark/run.sh
+++ pyspark/run.sh
@@ -16,16 +16,15 @@ gsutil rm -rf gs://large_scale_data/out
 
 
 ## create the cluster
-gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-west1 --zone europe-west1-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project large-scale-data-401112
+gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-central2 --zone europe-central2-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project large-scale-data-401112
 
 
 ## run
 ## (suppose that out directory is empty !!)
-gcloud dataproc jobs submit pyspark --region europe-west1 --cluster cluster-a35a gs://large_scale_data/pagerank-notype.py  -- gs://large_scale_data/small_page_links.nt 3
+gcloud dataproc jobs submit pyspark --region europe-central2 --cluster cluster-a35a gs://large_scale_data/pagerank-notype.py  -- gs://large_scale_data/small_page_links.nt 3
 
 ## access results
-gsutil cat gs://large_scale_data/out/pagerank_data_10/part-r-00000
+# gsutil cat gs://large_scale_data/out/pagerank_data_10/part-r-00000
 
 ## delete cluster...
-gcloud dataproc clusters delete cluster-a35a --region europe-west1
-
+gcloud dataproc clusters delete cluster-a35a --region europe-central2
