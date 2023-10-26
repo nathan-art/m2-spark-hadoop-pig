c763a32af529fce26cd9fab4ce4544235db7a221
nihelbelhadjkacem <nihelbelhadjkacem13@gmail.com>
2023-10-26 11:11:01 +0000

    final version of diagram

--- diagramme.py
+++ diagramme.py
@@ -1,31 +1,24 @@
 import matplotlib.pyplot as plt
 
+
 with open ('results.txt', 'r') as fichier :
      temps_execution=[]
      nombre_workers=[]
      for ligne in fichier:
-         mots = ligne.split()
-         nb_workers = int(mots[mots.index("workers") - 1])
-         temps = int(mots[mots.index("seconds") - 1])
-         nombre_workers.append(nb_workers)
-         temps_execution.append(temps)
+          mots = ligne.split()
+          nb_workers = int(mots[mots.index("workers") - 1])
+          temps= int(mots[mots.index("seconds.") - 1])
+          nombre_workers.append(nb_workers)
+          temps_execution.append(temps)
 
-#plt.grid(True)
-# with open ('results_pyspark.txt', 'r') as fichier 
-#      temps_dex2=[]
-#      workers_nbr2=[]
-#      for ligne in fichier:
-#         mots = ligne.split()
-#         nb_workers = int(mots[mots.index("workers") - 1])
-#         temps = int(mots[mots.index("seconds") - 1])
-#         nombre_workers.append(nb_workers)
-#         temps_execution.append(temps)
 
-plt.plot(nombre_workers, temps_execution, marker='o')
-#plt.plot(nombre_workers2, temps_execution2, marker='x')
+temps_execution2=[27.8, 38.13, 43.51]
+
+
+plt.plot(nombre_workers, temps_execution, marker='o',label='pig')
+plt.plot(nombre_workers, temps_execution2, marker='x',label='spark')
 plt.xlabel('Nombre de Workers')
-
-plt.ylabel('Temps d\'exécution (secondes)')
+plt.ylabel('Temps d\'exécution (minutes)')
 plt.title('Diagramme Temps d\'exécution en fonction du Nombre de Workers')
-
-plt.show()
+plt.legend(loc='upper right')
+plt.savefig("diagramme.jpeg") 
