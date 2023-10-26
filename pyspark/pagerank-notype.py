8b8543f2da5286f2fcf814a80d8eeedf112d2d08
nathan-art <58117140+nathan-art@users.noreply.github.com>
2023-10-26 10:03:40 +0200

    adding comments for pagerank-notype.py

--- pyspark/pagerank-notype.py
+++ pyspark/pagerank-notype.py
@@ -38,14 +38,14 @@ from pyspark.sql import SparkSession
 # removed typing for compatibility with Spark 3.1.3
 # typing ok with spark 3.3.0
 
-def computeContribs(urls, rank) :
+def computeContribs(urls, rank):
     """Calculates URL contributions to the rank of other URLs."""
     num_urls = len(urls)
     for url in urls:
         yield (url, rank / num_urls)
 
 
-def parseNeighbors(urls) :
+def parseNeighbors(urls):
     """Parses a urls pair string into urls pair."""
     parts = re.split(r'\s+', urls)
     return parts[0], parts[2]
@@ -53,7 +53,7 @@ def parseNeighbors(urls) :
 
 if __name__ == "__main__":
 
-
+    # this file takes 3 parameters, the file where the pagerank will be executed, the number of pagerank iterations and the number of workers in the cluster used for the execution of this python file
     if len(sys.argv) != 4:
         print("Usage: pagerank <file> <iterations> <num_workers>", file=sys.stderr)
         sys.exit(-1)
@@ -66,13 +66,13 @@ if __name__ == "__main__":
     bucket = "gs://large_scale_data"
 
     # Initialize the spark context.
-    spark = SparkSession\
-        .builder\
-        .appName("PythonPageRank")\
+    spark = SparkSession \
+        .builder \
+        .appName("PythonPageRank") \
         .getOrCreate()
 
+    # This is where the timestamp of the pagerank execution begins
     start_program_timestamp = time.time()
-
 
     # Loads in input file. It should be in format of:
     #     URL         neighbor URL
@@ -87,14 +87,14 @@ if __name__ == "__main__":
     # Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
     ranks = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
 
-    # partitioning of links
-    links = links.partitionBy(numPartitions = None)
+    # Partitioning of links
+    links = links.partitionBy(numPartitions=None)
 
     # Calculates and updates URL ranks continuously using PageRank algorithm.
     for iteration in range(int(sys.argv[2])):
-        # partitioning of ranks
-        ranks = ranks.partitionBy(numPartitions = None)
-        
+        # Partitioning of ranks
+        ranks = ranks.partitionBy(numPartitions=None)
+
         # Calculates URL contributions to the rank of other URLs.
         contribs = links.join(ranks).flatMap(lambda url_urls_rank: computeContribs(
             url_urls_rank[1][0], url_urls_rank[1][1]  # type: ignore[arg-type]
@@ -102,31 +102,29 @@ if __name__ == "__main__":
 
         # Re-calculates URL ranks based on neighbor contributions.
         ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15)
-    
-    #write and save pyspark results in a bucket 
-    ranks.coalesce(1).saveAsTextFile(bucket+"/pyspark_result_") 
+
+    # Write and save pyspark results in a bucket
+    ranks.coalesce(1).saveAsTextFile(bucket + "/pyspark_result_")
 
     print("Résultats pyspark écrits")
 
-
     spark.stop()
+
+    # This is where the timestamp of the pagerank execution ends
     finish_program_timestamp = time.time()
-    
+
     total_time_elapsed = finish_program_timestamp - start_program_timestamp
 
-    #for (link, rank) in ranks.collect():
-    #    print("%s has rank: %s." % (link, rank))
 
     statistics_time['start_program_timestamp'] = start_program_timestamp
     statistics_time['finish_program_timestamp'] = finish_program_timestamp
     statistics_time['total_time_elapsed'] = finish_program_timestamp - start_program_timestamp
 
-    #for (link, rank) in ranks.collect():
-    #    print("%s has rank: %s." % (link, rank))
-
-    statistics_filename = 'pyspark_statistics_num_workers_'+sys.argv[3]+'.json'
+    # the begin, end and total duration of the pagerank execution is stored in a json file, because it is simpler to collect to make the comparison graph of duration
+    statistics_filename = 'pyspark_statistics_num_workers_' + sys.argv[3] + '.json'
 
     with open(statistics_filename, 'w+') as outfile:
         outfile.write(json.dumps(statistics_time))
 
-    call(["gsutil" ,"cp",statistics_filename ,bucket])
+    # copy the json file in the bucket
+    call(["gsutil", "cp", statistics_filename, bucket])
