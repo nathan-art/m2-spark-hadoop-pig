2fa7f510f9b467b5ad08d63e56b2003ccf22da44
nathan-art <58117140+nathan-art@users.noreply.github.com>
2023-10-26 10:11:49 +0200

    add comment for run.sh

--- pyspark/run.sh
+++ pyspark/run.sh
@@ -1,27 +1,23 @@
-## Pour 2 workers
+## For a cluster with 2 workers
 
 ## copy data
-gsutil cp small_page_links.nt gs://large_scale_data/
-
-#pour utiliser les grosses données : 
+# To use all the data (download only 1 time in the bucket because it is an heavy file !) :
 # gsutil cp gs://public_lddm_data/page_links_en.nt.bz2 gs://large_scale_data/
 
-## copy pig code
+## copy pyspark code
 gsutil cp pagerank-notype.py gs://large_scale_data/
 
 ## Clean out directory
 gsutil rm -rf gs://large_scale_data/pyspark_result_
 
-
 ## create the cluster
 gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-central2 --zone europe-central2-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project large-scale-data-401112
 
-
 ## run
 ## (suppose that out directory is empty !!)
-gcloud dataproc jobs submit pyspark --region europe-central2 --cluster cluster-a35a gs://large_scale_data/pagerank-notype.py  -- gs://large_scale_data/small_page_links.nt 3 2
+gcloud dataproc jobs submit pyspark --region europe-central2 --cluster cluster-a35a gs://large_scale_data/pagerank-notype.py  -- gs://large_scale_data/page_links_en.nt.bz2 3 2
 
-## access results
+## display pagerank results in the console
 # gsutil cat gs://large_scale_data/out/pagerank_data_10/part-r-00000
 
 ## delete cluster...
